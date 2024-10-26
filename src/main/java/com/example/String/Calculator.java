cceac436f5042bf482df82dc3667b897e948025b
Pooja Narale <pooja.narale@humancloud.co.in>
2024-10-26 10:51:34 +0530

    Test Case Pass for adding new Line

--- src/main/java/com/example/String/Calculator.java
+++ src/main/java/com/example/String/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
     {
         if(!text.equals(""))
         {
-
             if(containsNegative(splitNumbers(text)))
             {
                 throw new IllegalArgumentException("Can Not Add Negative Number");
@@ -21,8 +20,20 @@ public class Calculator {
 
     public static String[] splitNumbers(String text)
     {
-        return text.split(",");
+
+        if(text.contains("\n"))
+        {
+            return text.split("\n");
+        }
+        else {
+            return text.split(",");
+        }
+          // return text.split(",");
+
+
+
     }
+
     public static boolean containsNegative(String arr[])
     {
         for(int i=0;i<arr.length;i++)
@@ -34,20 +45,29 @@ public class Calculator {
         }
         return false;
     }
+
     public static int stringToInt(String number)
     {
         return Integer.parseInt(number);
     }
-    public static int sumOfString(String str) {
+
+    public static int sumOfString(String str)
+    {
         int sum = 0;
+
         // Assuming splitNumbers is a method that splits the string into an array of numbers as strings
         String[] strArray = splitNumbers(str);
-        for (int i = 0; i < strArray.length; i++) {
-            sum = sum + stringToInt(strArray[i]);
+
+
+        for(int i=0;i<strArray.length;i++)
+        {
+            sum=sum+stringToInt(strArray[i]);
         }
         return sum;
 
-    }
 
+
+
+    }
 
 }
